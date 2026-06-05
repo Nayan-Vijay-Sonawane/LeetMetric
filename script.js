f396c86ca88effddd6b65a04785e9b0fa3dd26d9
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-05 10:06:59 +0000

    Creating the Scripting part of the project in the main file

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ document.addEventListener("DOMContentLoaded", function(){
     const easyProgressCircle = document.querySelector(".easy-progress");
     const mediumProgressCircle = document.querySelector(".medium-progress");
     const hardProgressCircle = document.querySelector(".hard-progress");
+    const easyLabel = document.getElementById("easy-label");
+    const mediumLabel = document.getElementById("medium-label");
+    const hardLabel = document.getElementById('hard-label');
+    const cardStatsContainer = document.querySelector("stats-card");
 
 })
