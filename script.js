cc824a1ba600cab7a0021c76e085a3183acf4930
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-05 10:30:59 +0000

    Creating the Scripting part of the project in the main file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-    
+
     const searchButton = document.getElementById("search-btn");
     const usernameInput = document.getElementById("user-input");
     const statsContainer = document.querySelector(".user-stats-container");
@@ -11,6 +11,22 @@ document.addEventListener("DOMContentLoaded", function(){
     const hardLabel = document.getElementById('hard-label');
     const cardStatsContainer = document.querySelector("stats-card");
 
+    // return true or false based on regex
+    function validateUsername(username){
+        if(username.trim() === ""){
+            alert("Username should not be empty");
+            return false;
+        }
+
+        const regex = /^[a-zA-Z0-9_-]{1, 15}$/;
+        const isMatching = regex.test(username);
+        if(!isMatching){
+            alert("Invalid username");
+        }
+        return isMatching;
+    }
+
+    // fetch the value after clicking on search button
     searchButton.addEventListener('click', function(){
         const username = usernameInput.value;
         console.log("Logging username: ", username);
