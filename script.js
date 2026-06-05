9a156f983193a79d52776edaacde9976fc1259be
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-05 12:41:50 +0000

    Creating the Scripting part of the project in the main file

--- script.js
+++ script.js
@@ -34,10 +34,10 @@ document.addEventListener("DOMContentLoaded", function(){
 
             // const response = await fetch(url);
 
-            const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+            const proxyUrl = "https://cors-anywhere.herokuapp.com"
             const targetUrl = "https://leetcode.com/graphql/";
             const myHeaders = new Headers();
-            myHeaders.append("content/type", "application/json");
+            myHeaders.append("Content-Type", "application/json");
 
             const graphql = JSON.stringify({
                 query: "\n   query userSessionProgress($username: String!) {\n  allQuestionsCount {\n    difficulty\n   count\n  }\n  matchedUser(username: $username) {\n   submitStats {\n  acSubmissionNum difficulty\n   count\n  submissions\n   }\n  totalSubmissionNum  {\n  difficulty\n   count\n   submissions\n   }\n   }\n  }\n}\n   ", 
@@ -48,15 +48,15 @@ document.addEventListener("DOMContentLoaded", function(){
                 method: "POST",
                 headers: myHeaders,
                 body: graphql,
-                redirect: "Follow"
+                redirect: "follow"
             };
 
-            const response = await fetch(proxyUrl+targetUrl, requestOptions);
+            const response = await fetch(proxyUrl + targetUrl, requestOptions);
             if(!response.ok){
                 throw new Error("Unable to fetch the data");
-                const data = await response.json();
-                console.log("Logging data: ", data);
             }
+            const data = await response.json();
+            console.log("Logging data: ", data);
         }
         catch(error){
             statsContainer.innerHTML = `<p>No data Found</p>`;
