16ad471a0fcde6e0898ce97f647013bdd6a8a00d
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-06-06 18:08:32 +0000

    Creating the Scripting part of the project in the main file

--- script.js
+++ script.js
@@ -76,7 +76,9 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
     function updateProgress(solved, total, label, circle){
-
+        const progressDegree = (solved/total)*100;
+        circle.style.setProperty("--progress-degree", `${progressDegree}%`);
+        label.textContent = `${solved}/${total}`;
     }
 
     function displayUserData(parsedData){
